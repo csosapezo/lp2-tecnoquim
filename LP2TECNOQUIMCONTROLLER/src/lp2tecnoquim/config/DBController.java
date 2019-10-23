941f30b28b16624274aa66930ac89f351bd9d712
Carlos Sosa <55820612+csosapezo@users.noreply.github.com>
2019-10-23 13:36:08 -0500

    Añadidos métodos hasta DetalleAlmacenProducto

--- LP2TECNOQUIMCONTROLLER/src/lp2tecnoquim/config/DBController.java
+++ LP2TECNOQUIMCONTROLLER/src/lp2tecnoquim/config/DBController.java
@@ -5,6 +5,9 @@
  */
 package lp2tecnoquim.config;
 
+import java.util.ArrayList;
+import lp2tecnoquim.model.*;
+
 /**
  *
  * @author alulab14
@@ -13,7 +16,51 @@ public abstract class DBController {
     
       private static final DAOFactory daoFactory = DAOFactory.getDAOFactory();
       
+      public void insertarAlmacen(Almacen almacen){
+          daoFactory.getAlmacenDAO().insertar(almacen);
+      }
       
-    
-    
+      public void actualizarAlmacen(Almacen almacen){
+          daoFactory.getAlmacenDAO().actualizar(almacen);
+      }
+      
+      public void eliminarAlmacen(int idAlmacen){
+          daoFactory.getAlmacenDAO().eliminar(idAlmacen);
+      }
+      
+      ArrayList<Almacen> listarAlmacen(){
+          return daoFactory.getAlmacenDAO().listar();
+      }
+      
+      public void insertarDetalleAlmacenInsumo(DetalleAlmacenInsumo detalleAlmacenInsumo){
+          daoFactory.getDetalleAlmacenInsumoDAO().insertar(detalleAlmacenInsumo);
+      }
+      
+      public void actualizarDetalleAlmacenInsumo(DetalleAlmacenInsumo detalleAlmacenInsumo){
+          daoFactory.getDetalleAlmacenInsumoDAO().actualizar(detalleAlmacenInsumo);
+      }
+      
+      public void eliminarDetalleAlmacenInsumo(int id){
+          daoFactory.getDetalleAlmacenInsumoDAO().eliminar(id);
+      }
+      
+      ArrayList<DetalleAlmacenInsumo> listarDetalleAlmacenInsumo(){
+          return daoFactory.getDetalleAlmacenInsumoDAO().listar();
+      }
+      
+      public void insertarDetalleAlmacenProducto(DetalleAlmacenProducto detalleAlmacenProducto){
+          daoFactory.getDetalleAlmacenProductoDAO().insertar(detalleAlmacenProducto);
+      }
+      
+      public void actualizarDetalleAlmacenProducto(DetalleAlmacenProducto detalleAlmacenProducto){
+          daoFactory.getDetalleAlmacenProductoDAO().actualizar(detalleAlmacenProducto);
+      }
+      
+      public void eliminarDetalleAlmacenProducto(int id){
+          daoFactory.getDetalleAlmacenProductoDAO().eliminar(id);
+      }
+      
+      ArrayList<DetalleAlmacenProducto> listarDetalleAlmacenProducto(){
+          return daoFactory.getDetalleAlmacenProductoDAO().listar();
+      }
 }
