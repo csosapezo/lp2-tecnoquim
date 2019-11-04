f7d090fa395adc717f4826b29db8c3efbe55c623
Carlos Sosa <c.sosa@pucp.edu.pe>
2019-11-04 10:32:41 -0500

    listar por periodo

--- LP2TECNOQUIMCONTROLLER/src/lp2tecnoquim/mysql/ProyeccionVentaMySQL.java
+++ LP2TECNOQUIMCONTROLLER/src/lp2tecnoquim/mysql/ProyeccionVentaMySQL.java
@@ -66,12 +66,13 @@ public class ProyeccionVentaMySQL implements ProyeccionVentaDAO{
     }
 
     @Override
-    public ArrayList<ProyeccionVenta> listar() {
+    public ArrayList<ProyeccionVenta> listar(java.util.Date periodo) {
         ArrayList<ProyeccionVenta> proyeccion = new ArrayList<>();
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(DBManager.url, DBManager.user, DBManager.password);
             cs = con.prepareCall("{call LISTAR_PROYECCION()}");
+            
             ResultSet rs = cs.executeQuery();
             while(rs.next()){
                 ProyeccionVenta  a = new ProyeccionVenta();
